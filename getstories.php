47f1190e7a4fd02c9cb89a127d9716fe4e56fc45
redpois0n <git@redpois0n>
2015-03-10 12:54:49 +0100

    Checks all available snaps

--- getstories.php
+++ getstories.php
@@ -1,5 +1,20 @@
-<?php 
+<?php
+require_once "config.php";
+require_once "snapchat.php";
 
+$snapchat = new Snapchat(USERNAME, $auth_token, false);
 
+$snapchat->login(USERNAME, PASSWORD);
+
+$stories = $snapchat->getFriendStories(false);
+
+foreach ($stories as $story) {
+	$id = $story->media_id;
+	$from = $story->username;
+	$mediaKey = $story->media_key;
+	$mediaIV = $story->media_iv;
+	
+	echo "Story found, ID: " . $id . ", from: " . $from . "\n";
+}
 
 ?>
