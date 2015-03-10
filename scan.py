f0d457fd562cd68b17dbb2db8ddb095fa2c3eab7
redpois0n <git@redpois0n>
2015-03-10 11:49:09 +0100

    Scanner work, download function implemented

--- scan.py
+++ scan.py
@@ -1,5 +1,19 @@
+import urllib.request
+
 def main():
     print("Running scanner")
+    
+def download(url, save):
+    response = urllib.request.urlopen(url)
+    data = response.read()      # a `bytes` object
+    
+    if save:
+        f = open("test.txt", "wb")
+        f.write(data)
+        f.close()
+    else:
+        return data.decode("utf-8")
+        
 
 if __name__ == "__main__":
     main()
